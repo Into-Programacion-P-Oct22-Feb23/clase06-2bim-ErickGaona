01b35c65d6236461461eed603e1d877999c8392b
Erick Gaona <Erickgaona625@gmail.com>
2023-01-18 15:00:48 -0500

    Actividad en clase f

--- Ejemplos/src/ejemplos/Ejemplo02.java
+++ Ejemplos/src/ejemplos/Ejemplo02.java
@@ -14,43 +14,42 @@ import java.util.Scanner;
  * @author reroes
  */
 public class Ejemplo02 {
-    
+
     public static void main(String[] args) {
         // TODO code application logic here
         Scanner entrada = new Scanner(System.in);
         System.out.println("Ingrese limite");
-        int valor=entrada.nextInt();
-        if(valor>0 ){
-        int valorA = obtenerNumero(valor); // 3
-        int valorB = obtenerNumero(valor); // 2
-        int suma = obtnerSuma(valorA, valorB);
-        
-        System.out.printf("La suma de %d + %d es igual a: %d\n", 
-                valorA,
-                valorB,
-                suma);
-        
-        }else{
-            if(valor<0){
-        System.out.println("ERROR");
-    
-    
-    }
+        int valor = entrada.nextInt();
+        if (valor > 0) {
+            int valorA = obtenerNumero(valor); // 3
+            int valorB = obtenerNumero(valor); // 2
+            int suma = obtnerSuma(valorA, valorB);
+
+            System.out.printf("La suma de %d + %d es igual a: %d\n",
+                    valorA,
+                    valorB,
+                    suma);
+
+        } else {
+            
+                System.out.println("ERROR");
+
+            
         }
     }
-    
+
     public static int obtenerNumero(int c) {
         // objeto generador de números aleatorios
         SecureRandom numerosAleatorios = new SecureRandom();
-        
+
         // Returns a pseudorandom, uniformly distributed int value 
         // between 0 (inclusive) and the specified value (exclusive)
         int valorAleatorio = numerosAleatorios.nextInt(c);
         return valorAleatorio;
     }
-    
-    public static int obtnerSuma(int a, int b){
+
+    public static int obtnerSuma(int a, int b) {
         return a + b;
     }
-    
+
 }
